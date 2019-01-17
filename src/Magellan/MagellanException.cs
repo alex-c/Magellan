8d5b0fa61fd4f7f9085ab2817cc4ed67786f64eb
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-01-17 17:45:59 +0100

    Added exception classes

---
+++ src/Magellan/MagellanException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Magellan
+{
+    /// <summary>
+    /// Generic exception class for exceptions from the Magellan library.
+    /// </summary>
+    public class MagellanException : Exception
+    {
+        public MagellanException() { }
+
+        public MagellanException(string message) : base(message) { }
+
+        public MagellanException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected MagellanException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ src/Magellan/NoAvailableServiceInstanceException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Magellan
+{
+    /// <summary>
+    /// There is no healthy service instance available for the given service.
+    /// </summary>
+    public class NoAvailableServiceInstanceException : Exception
+    {
+        public NoAvailableServiceInstanceException() : this("There is no healthy service instance available for the given service.") { }
+
+        public NoAvailableServiceInstanceException(string service) : base($"There is no healthy service instance available for the service '{service}'.") { }
+
+        public NoAvailableServiceInstanceException(string service, Exception innerException) : base($"There is no healthy service instance available for the service '{service}'.", innerException) { }
+
+        protected NoAvailableServiceInstanceException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
